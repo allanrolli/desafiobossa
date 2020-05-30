d4ddcc5605e8cc87e4bf0c25702d41c6c8190bd8
Allan Rolli <allan.rolli@hipr.com.br>
2020-05-30 13:53:37 -0300

    user model

---
+++ src/model/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password_hash: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", User);
