1ac0fe5567b1243ba884670c64c91e24d2dc31ab
Allan Rolli <allan.rolli@hipr.com.br>
2020-06-01 02:27:52 -0300

    update documentation and gitignore

--- src/router.js
+++ src/router.js
@@ -4,13 +4,13 @@ const User = require("./controller/UserController");
 const authMiddleware = require("./middlewares/auth");
 const router = express.Router();
 
-router.post("/v1/authenticate", User.authenticate);
+router.post("/authenticate", User.authenticate);
 
-router.get("/v1/user", authMiddleware, User.findAll);
-router.get("/v1/tools", authMiddleware, Tools.findOne);
-router.post("/v1/tools", authMiddleware, Tools.store);
-router.post("/v1/user", authMiddleware, User.store);
-router.delete("/v1/tools/:id", authMiddleware, Tools.remove);
+router.get("/user", authMiddleware, User.findAll);
+router.get("/tools", authMiddleware, Tools.findOne);
+router.post("/tools", authMiddleware, Tools.store);
+router.post("/user", authMiddleware, User.store);
+router.delete("/tools/:id", authMiddleware, Tools.remove);
 
 router.get("/", (req, res) => {
   console.log("Entrei aqui");
