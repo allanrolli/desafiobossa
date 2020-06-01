49938cb505fd259add62d02c8709e8b29e8c2df0
Allan Rolli <allan.rolli@hipr.com.br>
2020-06-01 13:06:51 -0300

    update documentation

--- src/router.js
+++ src/router.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const Tools = require("./controller/ToolController");
 const User = require("./controller/UserController");
@@ -15,9 +16,6 @@ router.post("/user", authMiddleware, User.store);
 router.delete("/tools/:id", authMiddleware, Tools.remove);
 
 router.get("/", (req, res) => {
-  console.log("Entrei aqui");
-
-  return res.send("Esta é a página principal");
+  res.sendFile(path.join(__dirname + "/docs/index.html"));
 });
-
 module.exports = router;
